e6731d371707a3f4bbf82ab31f63d6a5ddefdfa6
ksingh <ksinghofficial@proton.me>
2023-09-05 02:11:01 +0530

    fixed hire button & other bugs

--- src/app/hire/page.jsx
+++ src/app/hire/page.jsx
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-// import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -43,7 +42,7 @@ export default function Album() {
         <Toolbar>
           {/* <CameraIcon sx={{ mr: 2 }} /> */}
           <Typography variant="h6" color="inherit" noWrap>
-           Hire Me
+            Hire Me
           </Typography>
         </Toolbar>
       </AppBar>
@@ -64,12 +63,10 @@ export default function Album() {
               color="text.primary"
               gutterBottom
             >
-             The Best Dev On The Planet.
+              The Best Dev On The Planet.
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
+              “No one in the brief history of computing has ever written a piece of perfect software. It’s unlikely that you’ll be the first.”
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -83,91 +80,91 @@ export default function Album() {
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
+
           <Grid container spacing={4}>
-            {/* {cards.map((card) => ( */}
-              <Grid item  xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '90%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '50.25%',
-                    }}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnpm-assets.fiverrcdn.com%2Fassets%2Flayout%2Ffiverr-og-logo.99c4dbf.png&f=1&nofb=1&ipt=30423d73c5119fcb78a5b2bee6ff19563983c8251c307b845dd3cf411cb2ffb4&ipo=images"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      fiverr.
-                    </Typography>
-                    <Typography>
-                    I'm available on  fiverr.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Hire Me</Button>
-                    {/* <Button size="small">Chat With Me</Button> */}
-                  </CardActions>
-                </Card>
-              
-              </Grid>
-            {/* ))} */}
-            <Grid item  xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2021%2F04%2FUpwork-Symbol.png&f=1&nofb=1&ipt=e1f5bc3fb57a6e3da870c0c8d51be63256901555085ac82858810efb7b9a2944&ipo=images"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      UpWork
-                    </Typography>
-                    <Typography>
+
+          <Grid item xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: '40.25%',
+                  }}
+                  image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnpm-assets.fiverrcdn.com%2Fassets%2Flayout%2Ffiverr-og-logo.99c4dbf.png&f=1&nofb=1&ipt=82f0966a0a5f703bb603911e3e1be70908326b78149dc8151164dd1170fee4cb&ipo=images"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  Fiverr.
+                  </Typography>
+                  <Typography>
+                    I'm available on Fiverr.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Hire Me</Button>
+                  {/* <Button size="small">Chat With Me</Button> */}
+                </CardActions>
+              </Card>
+
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: '56.25%',
+                  }}
+                  image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2021%2F04%2FUpwork-Symbol.png&f=1&nofb=1&ipt=e1f5bc3fb57a6e3da870c0c8d51be63256901555085ac82858810efb7b9a2944&ipo=images"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    UpWork
+                  </Typography>
+                  <Typography>
                     I'm available on  UpWork.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Hire Me</Button>
-                    {/* <Button size="small">Chat With Me</Button> */}
-                  </CardActions>
-                </Card>
-              
-              </Grid>
-              <Grid item  xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '40.25%',
-                    }}
-                    image="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcustomercarecontacts.com%2Fwp-content%2Fuploads%2F2017%2F07%2Ffreelancer-logo.jpg&f=1&nofb=1&ipt=a46861a67f5bab20a0261300d1cd8817b9916618fed7cf745bc6aa8c95d5729b&ipo=images"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Hire Me</Button>
+                  {/* <Button size="small">Chat With Me</Button> */}
+                </CardActions>
+              </Card>
+
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: '40.25%',
+                  }}
+                  image="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcustomercarecontacts.com%2Fwp-content%2Fuploads%2F2017%2F07%2Ffreelancer-logo.jpg&f=1&nofb=1&ipt=a46861a67f5bab20a0261300d1cd8817b9916618fed7cf745bc6aa8c95d5729b&ipo=images"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
                     Freelancer.
-                    </Typography>
-                    <Typography>
-                      I'm available on freelancer.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Hire Me</Button>
-                    {/* <Button size="small">Chat With Me</Button> */}
-                  </CardActions>
-                </Card>
-              
-              </Grid>
+                  </Typography>
+                  <Typography>
+                    I'm available on freelancer.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Hire Me</Button>
+                  {/* <Button size="small">Chat With Me</Button> */}
+                </CardActions>
+              </Card>
+
+            </Grid>
           </Grid>
         </Container>
       </main>
