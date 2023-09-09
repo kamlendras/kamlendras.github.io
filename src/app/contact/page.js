4d157ba67d1e63fa98fdfa771a439d2b98670870
ksingh <ksinghofficial@proton.me>
2023-09-10 03:39:48 +0530

    modified contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -69,15 +69,13 @@ export default function Checkout() {
       >
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            K Singh
+            Contact Me
           </Typography>
         </Toolbar>
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Contact Me
-          </Typography>
+         
           <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
             {steps.map((label) => (
               <Step key={label}>
