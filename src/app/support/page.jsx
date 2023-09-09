c26db779bb52ff1c0a68ed18e96e443346176065
ksingh <ksinghofficial@proton.me>
2023-09-10 03:36:29 +0530

    removed stack added grid for more responsiveness

--- src/app/support/page.jsx
+++ src/app/support/page.jsx
@@ -1,80 +1,96 @@
-"use client"
+"use client";
 import fish from "./fish.json";
 import Lottie from "lottie-react";
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Link from 'next/link'
-import Stack from '@mui/material/Stack';
-import MLink from '@mui/material/Link';
-import styles from "../styles/support.module.css"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import Stack from "@mui/material/Stack";
+import MLink from "@mui/material/Link";
+import styles from "../styles/support.module.css";
+import Grid from "@mui/material/Grid";
 const style = {
   height: 230,
 };
 export default function Types() {
   return (
-   <div className={styles.one}>
-       <Typography variant="h4" gutterBottom>
-       Get Involved
+    <div className={styles.one}>
+      <Typography variant="h4" gutterBottom>
+        Get Involved
       </Typography>
-    
+
       <Typography variant="body1" gutterBottom>
-     There are many methords by which you can support me ,
-     Whether you have ideas, translations, design changes, code cleaning, or real heavy code changes, help is always welcome. The more is done the better it gets!
-If you're interested in contributing, you can browse the issues or create a new one to discuss your feature idea. Every contribution is very welcome.
-
-     You can contribute to one of my originations
-   <MLink  href="https://github.com/codnap"  underline="always">  github.com/codnap </MLink> and 
-   <MLink  href="https://github.com/streetsfoods"  underline="always">  github.com/streetsfoods</MLink> .
-     
-
-
+        There are many methords by which you can support me , Whether you have
+        ideas, translations, design changes, code cleaning, or real heavy code
+        changes, help is always welcome. The more is done the better it gets! If
+        you're interested in contributing, you can browse the issues or create a
+        new one to discuss your feature idea. Every contribution is very
+        welcome. You can contribute to one of my originations
+        <MLink href="https://github.com/codnap" underline="always">
+          {" "}
+          github.com/codnap{" "}
+        </MLink>{" "}
+        and
+        <MLink href="https://github.com/streetsfoods" underline="always">
+          {" "}
+          github.com/streetsfoods
+        </MLink>{" "}
+        .
       </Typography>
-    
-    
+
       <Typography variant="h4" gutterBottom>
         Join Matrix Rooms
       </Typography>
       <Typography variant="body1" gutterBottom>
-      You can join matrix chat rooms of <MLink  href="https://matrix.to/#/#codnap:matrix.org"  underline="always">  #codnap.matrix.org  </MLink> <br/>
-      You can join matrix chat rooms of  <MLink  href="https://matrix.to/#/#SFI:matrix.org"  underline="always"> #SFI.matrix.org </MLink> 
+        You can join matrix chat rooms of{" "}
+        <MLink href="https://matrix.to/#/#codnap:matrix.org" underline="always">
+          {" "}
+          #codnap.matrix.org{" "}
+        </MLink>{" "}
+        <br />
+        You can join matrix chat rooms of{" "}
+        <MLink href="https://matrix.to/#/#SFI:matrix.org" underline="always">
+          {" "}
+          #SFI.matrix.org{" "}
+        </MLink>
       </Typography>
       <Typography variant="h4" gutterBottom>
         Donate
       </Typography>
-     
+
       <Typography variant="h6" gutterBottom>
-      If you like this project, please consider donating so I can keep this project running forever!
+        If you like this project, please consider donating so I can keep this
+        project running forever!
       </Typography>
-      <Stack direction="row" spacing={2}>
-      <Link href="https://www.patreon.com/KSingh" >
-  
-    <img src="/pat.png" 
-    height={80} 
-    // width={35} 
-    />
-    
-</Link>
 
-<Link href="https://www.buymeacoffee.com/ksingh" >
-  
-  <img src="bmc.svg" 
-  height={80} 
-  // width={35} 
-  />
-  
-</Link>
-<Link href="https://liberapay.com/KSingh" >
-<img src="https://img.shields.io/badge/liberapay-donate-yellow.svg?style=for-the-badge" 
-height={80} 
-// width={35} 
-/>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={12} lg={4}>
+          <Link href="https://www.patreon.com/KSingh">
+            <img
+              src="/pat.png"
+              height={80}
+            />
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={12} lg={3}>
+          <Link href="https://www.buymeacoffee.com/ksingh">
+            <img
+              src="bmc.svg"
+              height={80}
+            />
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={12} lg={3}>
+          <Link href="https://liberapay.com/KSingh">
+            <img
+              src="https://img.shields.io/badge/liberapay-donate-yellow.svg?style=for-the-badge"
+              height={80}
+            />
+          </Link>
+        </Grid>
+      </Grid>
 
-
-</Link>
-</Stack>
-<Lottie animationData={fish} loop={true}
-       style={style} />
+      <Lottie animationData={fish} loop={true} style={style} />
     </div>
   );
 }
